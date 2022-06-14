ecb8f71e3e36254ecb1e1185f07146c1e90016d9
sohampati <106694445+sohampati@users.noreply.github.com>
2022-06-13 22:06:05 -0400

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,126 +1,45 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'package:net_ninja_follow/quote_card.dart';
+void main() => runApp(MaterialApp(
+    home: QuoteList()
+));
 
-void main() {
-  runApp(const MaterialApp(
-    home: NinjaCard(),
-  ));
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class NinjaCard extends StatelessWidget {
-  const NinjaCard({Key? key}) : super(key: key);
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple'),
+  ];
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Ninja ID Card'),
+        title: const Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.cyan[400],
-        elevation: 0,
+        backgroundColor: Colors.redAccent,
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage('https://wallpaperaccess.com/full/4129901.jpg'),
-                radius: 40.0,
-              ),
-
-            ),
-            Divider(
-              height: 40.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.red,
-                letterSpacing: 2.0,
-
-
-
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Itachi Uchiha',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-
-
-
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Current Ninja Level',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 14.0,
-                letterSpacing: 2.0,
-
-
-
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '1000000',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-
-
-
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-
-
-              children: [
-
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-
-                ),
-                SizedBox(width: 38.0),
-                Text(
-
-                  'hateuchiha@strongestclan.com',
-                  style:TextStyle(
-                    color: Colors.grey[400],
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.0,
-
-                  ),
-
-                ),
-
-
-              ],
-            ),
-
-
-
-
-
-
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState((){
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
       ),
     );
   }
 }
-
 
