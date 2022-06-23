3082ca7266f2fd0e12d92efd465cd3760e9d4b5c
sohampati <106694445+sohampati@users.noreply.github.com>
2022-06-23 08:31:39 -0400

    Add files via upload

---
+++ loading.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getData() async {
+    var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/todos/1"));
+    // print(response.body);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Text('loading screen'),
+      ),
+    );
+  }
+}
